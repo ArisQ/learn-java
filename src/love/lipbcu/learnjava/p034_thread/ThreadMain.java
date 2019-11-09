e186aa9103629c231d284c9f4eba135dce9e2f25
ArisQ <qiaoli.pg@gmail.com>
2019-11-09 16:36:29 +0800

    中断线程

--- src/love/lipbcu/learnjava/p034_thread/ThreadMain.java
+++ src/love/lipbcu/learnjava/p034_thread/ThreadMain.java
@@ -1,7 +1,33 @@
 package love.lipbcu.learnjava.p034_thread;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class ThreadMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        Thread threadToInterrupt = new Thread(() -> {
+            int i = 0;
+            while (!Thread.currentThread().isInterrupted()) {
+                System.out.println("working " + i++);
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    // Thread.currentThread().isInterrupted() 不会被置位
+                    break;
+                }
+            }
+        });
+        threadToInterrupt.start();
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                threadToInterrupt.interrupt();
+            }
+        }, 1000);
+        threadToInterrupt.join();
+
         Runnable r = () -> {
             try {
                 for (int j = 0; j < 100; ++j) {
